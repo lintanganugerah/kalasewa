668cf4bef17ec12d4ee511cbddbdbff7e411843a
lintanganugerah <lintanganugerah4@gmail.com>
2024-04-27 17:09:28 +0100

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('login-seller');
+});
+
+Route::get('/daftar', function () {
+    return view('daftar-seller');
+});
+
+Route::get('/daftar/verifikasi', function () {
+    return view('daftar-kode-verifikasi');
+});
+
+Route::get('/daftar/informasiseller', function () {
+    return view('daftar-informasi-seller');
+});
+Route::get('/test', function () {
+    return view('test');
+});
